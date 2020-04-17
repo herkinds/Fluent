47449cd6bd4c503439a54132ed7febf8ad6c9f07
herkinds <58661318+herkinds@users.noreply.github.com>
2020-04-17 19:02:52 +0200

    Implemented FilePath

--- src/FluentFiles/FilePath.cs
+++ src/FluentFiles/FilePath.cs
@@ -1,11 +1,65 @@
-﻿using Herkinds.FluentFiles;
+﻿using Herkinds.FluentFiles.Nodes;
 using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
-namespace FluentFiles.Core
+namespace Herkinds.FluentFiles
 {
-    public sealed class FilePath  //: IPath
+    public sealed class FilePath : IPath
     {
+        public FilePath(DirectoryPath location, FileNode file)
+        {
+            Location = location ?? throw new ArgumentNullException(nameof(location));
+            File = file ?? throw new ArgumentNullException(nameof(file));
+        }
+
+        public DirectoryPath Location { get; }
+
+        public FileNode File { get; set; }
+
+        #region IPath
+        public INode this[int index]
+        {
+            get
+            {
+                if (index == Location.Count)
+                    return File;
+                else
+                    return Location[index - 1];
+            }
+        }
+
+        public int Count => Location.Count + 1;
+
+        public IEnumerator<INode> GetEnumerator()
+        {
+            yield return Location.Drive;
+            foreach (var folder in Location.Directories)
+                yield return folder;
+            yield return File;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetEnumerator();
+
+        public bool Exists()
+            => System.IO.File.Exists(this);
+
+        public void Create()
+            => System.IO.File.Create(this);
+        #endregion
+
+        #region Cast & ToString
+        public static implicit operator string(FilePath path)
+            => path?.ToString();
+
+        public static implicit operator FileInfo(FilePath path)
+            => new FileInfo(path);
+
+        public override string ToString()
+            => Path.Combine(this.Select(node => node.Name).ToArray());
+        #endregion
     }
 }
